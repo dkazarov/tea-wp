da4a074e7e133638dcfda1dd48c82142ebad7c42
Dmytro <d.kazarov1361@gmail.com>
2025-02-20 17:20:02 +0200

    header: add js script to toggle search

--- src/js/main.js
+++ src/js/main.js
@@ -1,4 +1,12 @@
 import { burgerOpen, burgerClose } from './js-components/burger';
 
+const inputSearch = document.querySelector('#search-input');
+
+const search = document.querySelector('#search-btn').addEventListener('click', () => {
+	inputSearch.classList.toggle('max-w-180');
+});
+
+search();
+
 burgerOpen();
 burgerClose();
